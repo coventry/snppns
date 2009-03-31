5d6df3fdcd050093c389c6e16fc965098ec3809d
Walker Hale <hale@bcm.edu>
2009-03-31 17:04:22 -0500

    Add support for setting PHRED_PARAMETER_FILE and LD_LIBRARY_PATH.

--- config.bash
+++ config.bash
@@ -14,7 +14,19 @@ originalphredsourcedir=~/src/phred/source
 BLAT=~/bin/i386/blat
 
 # Path to local sage executable (if using sage)
-LOCALSAGE=~/src/sage/sage
+LOCALSAGEDIR=/code/production/SnppnS/sage-3.4
+LOCALSAGE=${LOCALSAGEDIR}/sage
+
+# Location of phred parameter file.
+# If not set, will default as follows:
+# * the current value of the PHRED_PARAMETER_FILE environment variable
+# * the old file that is bundled with phred 0.020425.c
+# export PHRED_PARAMETER_FILE=~/phredpar.dat
+
+# Path to directory containing BLAS and LAPACK.
+# May not be required for some deployments of scipy.
+# If using sage, just uncomment this line.
+# export LD_LIBRARY_PATH=${LOCALSAGEDIR}/local/lib
 
 # Path to the python executable
 PYTHON=${reldir}/sage.bash # /usr/local/bin/python
